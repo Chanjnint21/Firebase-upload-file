a2683d4be7f5d27aa54a10dc74086608c03d5268
Chanjnint21 <limchanjnint@gmail.com>
2023-07-19 17:12:28 +0700

    Added source code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <template v-for="path in images">
+      <CloudImage :key="path" :path='`folder/${path}`'/>
+    </template>
+    <input type="file" ref ="myfile">
+    <button @click="upload">upload</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { storage } from './firebase'
+import { ref,uploadBytes } from 'firebase/storage'
+import CloudImage from './components/CloudImage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CloudImage
+  },
+  data() {
+    return {
+      images: []
+    }
+  },
+  methods: {
+    upload: function() {
+      const file = this.$refs.myfile.files[0]
+      const fileName = `TripImg0${this.images.length + 1}.jpg`    
+      const storageRef = ref(storage, `folder/${fileName}`)
+      uploadBytes(storageRef, file).then((snapshot) => {
+        console.log(snapshot)
+        // Add the uploaded image to the images array
+        this.images.push(fileName)
+      })
+    }
   }
 }
 </script>
